79e0a8c1a649de5aebb8983457afa5b25271ea7f
kirill kreysmann <kreysmann.kirya@mail.ru>
2022-11-13 17:50:18 +0300

    add css-class background

--- sand-mine/src/main/resources/static/js/main.js
+++ sand-mine/src/main/resources/static/js/main.js
@@ -1,6 +1,7 @@
 import  {createApp} from 'vue'
 import App from "./pages/App.vue"
 import Temp from "./pages/TempPageForTable.vue"
+import TempBackPage from './pages/TempPageForBackground.vue'
 
 // createApp(App).mount('#app')
-createApp(Temp).mount('#app')
+createApp(TempBackPage).mount('#app')
---
+++ sand-mine/src/main/resources/static/js/pages/TempPageForBackground.vue
@@ -0,0 +1,30 @@
+<template>
+<v-app>
+  <div class="background">
+    <span>ФИО: Иванов Иван Иванович</span>  <br>
+    <span>Телефон: +70000000000</span>  <br>
+    <span>Почта: ivanov@mail.ru</span>  <br>
+    <span>Должность: Администратор</span>  <br>
+    <span>Паспортные данные: 6666 666666</span>  <br>
+    <span>Пароль: *******</span>  <br>
+    <button>Мои смены</button>
+    <button>Мои смены</button>
+    <button>Мои смены</button>
+    <button>Мои смены</button>
+    <button>Мои смены</button>
+    <button>Мои смены</button>
+    <button>Мои смены</button>
+  </div>
+</v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+
+</style>

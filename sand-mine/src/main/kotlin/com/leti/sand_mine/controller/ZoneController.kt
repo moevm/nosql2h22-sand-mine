4d21cb8fa24cb603fc047bd16246833828554e87
root <root@nikita>
2022-11-16 07:27:05 +0300

    add zone nnode

--- sand-mine/src/main/kotlin/com/leti/sand_mine/controller/ZoneController.kt
+++ sand-mine/src/main/kotlin/com/leti/sand_mine/controller/ZoneController.kt
@@ -1,9 +1,12 @@
 package com.leti.sand_mine.controller
 
+import com.leti.sand_mine.repository.ZoneRepository
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("api/zone")
-class ZoneController {
+class ZoneController (
+    private val zoneRepository: ZoneRepository
+) {
 }
--- sand-mine/src/main/kotlin/com/leti/sand_mine/repository/ZoneRepository.kt
+++ sand-mine/src/main/kotlin/com/leti/sand_mine/repository/ZoneRepository.kt
@@ -1,4 +1,6 @@
 package com.leti.sand_mine.repository
 
-interface ZoneRepository {
-}
+import com.leti.sand_mine.domain.Zone
+import org.springframework.data.neo4j.repository.Neo4jRepository
+
+interface ZoneRepository : Neo4jRepository<Zone, Long>

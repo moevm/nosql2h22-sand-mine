1c070a243f2e31fc133f1e590d6bdf0a002f9d94
root <root@nikita>
2022-11-18 22:14:51 +0300

    small refactoring; added Import/Export Page

--- sand-mine/src/main/resources/static/js/main.js
+++ sand-mine/src/main/resources/static/js/main.js
@@ -3,6 +3,9 @@ import App from "./pages/App.vue"
 import Temp from "./pages/TempPageForTable.vue"
 import TempBackPage from './pages/TempPageForBackground.vue'
 import ProductionPage from './pages/ProductionPage.vue'
+import ImportExportPage from "./pages/ImportExportPage.vue"
+import StaffListPage from "./pages/StaffListPage.vue"
 
+
+// createApp(App).mount('#app')
 createApp(Temp).mount('#app')
-// createApp(TempBackPage).mount('#app')
